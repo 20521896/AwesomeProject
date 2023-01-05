8cd977922b6fc29efe2099850979e15af137e6ee
Vitality <20521896@gm.uit.edu.vn>
2023-01-05 15:28:14 +0700

    OkLong

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
+import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView,StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const App=() => {
+  const [defaultRating, setdefaultRating] = useState(2)
+  const [maxRating, setmaxRating] = useState([1,2,3,4,5])
+  const starImgFilled='https://raw.githubusercontent.com/tranhonghan/images/main/star_filled.png'
+  const starImgCorner='https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png'
+  const CustomRatingBar= () =>{
+    return (
+    <View style={styles.customRatingBarStyle}>
+      {
+        maxRating.map((item, key) =>{
+          return(
+            <TouchableOpacity
+            activeOpacity={0.7}
+            key={item}
+            onPress={()=> setdefaultRating(item)}>
+              <Image  style={styles.starImgStyle}
+              source={
+                item <= defaultRating
+                ? {uri: starImgFilled}
+                : {uri: starImgCorner}}         
+              /></TouchableOpacity>        
+          )
+        })
+      }     
     </View>
+    )
+  }
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textStyle}>Please Rate me</Text>
+      <CustomRatingBar/>
+    </SafeAreaView>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding:10,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 23
+  },
+  customRatingBarStyle:{
+    justifyContent:'center',
+    flexDirection:'row',
+    marginTop:30
+  },
+  starImgStyle:{
+    width: 40,
+    height: 40,
+    resizeMode:'cover'
+  }
 });
+
+export default App;
